1d3980c3002c9573bfddd814f8cfddd9a8fe5e5c
孔建华08490 <kjh08490@ly.com>
2015-04-20 19:10:57 +0800

    首页

--- WebRoot/clutureListAction.js
+++ WebRoot/clutureListAction.js
@@ -75,8 +75,65 @@ function initBanner(newsList) {
             $('#cluture_type').html("<strong>类型:</strong>" + newsList[options.currSlide].typelx);
             $('#cluture_desc').text(newsList[options.currSlide].desc);
             // 赋值a标签href
-            $('#cluture_url').attr('href',(newsList[options.currSlide].newsId));
+            $('#cluture_url').attr('href', (newsList[options.currSlide].newsId));
         },
         pauseOnPagerHover: 0 // true to pause when hovering over pager link
     });
 };
+
+/**
+ * 新闻
+ */
+function clickGetNewsList() {
+    $.ajax({
+        // url : "http://yundong.shenghuo365.net/ActiveHandler.ashx",
+        url: "cluturelistdata.json",
+        type: "POST",
+        contentType: "application/json", // 必须有
+        dataType: "json", // 表示返回值类型，不必须
+        data: JSON.stringify({
+            "request": {
+                "body": {
+                    "clientInfo": {
+                        "clientIp": "10.228.237.141",
+                        "deviceId": "68a116b07394858b",
+                        "networktype": "wifi",
+                        "refId": "5866720",
+                        "versionNumber": "1.0",
+                        "versionType": "android"
+                    },
+                    "activeId": "467"
+                },
+                "header": {
+                    "accountID": "c26b007f-c89e-431a-b8cc-493becbdd8a2",
+                    "digitalSign": "7c0a13565e8a110eb4e3875708c056c4",
+                    "reqTime": "1429289137608",
+                    "serviceName": "GetActiveDetailById",
+                    "version": "20111128102912"
+                }
+            }
+        }), // 相当于请求的json
+        success: function (data) {
+            // 将json转string
+            var jsonText = JSON.stringify(data);
+            alert(jsonText);
+            // 解析json里面的数据
+            var response = data.response;
+            var newsList = data.response.body.newsList;
+            $.each(newsList, function (i, v) {
+                //alert(newsList[i].newsId);
+            });
+            var htmls = "";
+            $.each(newsList, function (i, v) {
+                if (i > 4) {
+                    return;
+                }
+                htmls = htmls + "<li> <p class='name'><strong>" + "作者：" + "</strong>" + newsList[i].author + "</p> <blockquote>" + newsList[i].content + "</blockquote> <p class='readmore' ><a target='_blank' href='" + newsList[i].newsId + "'><strong>" + "阅读详情" + "</strong></a></p> </li>";
+            });
+            $("#newslist").html(htmls);
+
+            // 重定向地址
+            //window.location.href = "../newsdetail/newsdetailpage.html?newsid=1"
+        }
+    })
+}
