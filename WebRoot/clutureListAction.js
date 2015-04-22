35435b2b4766b5c94e4f42d8a64683a079d8442e
孔建华08490 <kjh08490@ly.com>
2015-04-22 17:42:10 +0800

    页面简介、招聘

--- WebRoot/clutureListAction.js
+++ WebRoot/clutureListAction.js
@@ -54,6 +54,62 @@ function clickGetCluturesList() {
         }
     });
 }
+/**
+ * 获取公司简介信息
+ */
+
+function clickGetCompanyProfile() {
+
+    $.ajax({
+        // url : "http://yundong.shenghuo365.net/ActiveHandler.ashx",
+        url: "cluturelistdata.json",
+        type: "POST",
+        contentType: "application/json", // 必须有
+        dataType: "json", // 表示返回值类型，不必须
+        data: JSON.stringify({
+            "request": {
+                "body": {
+                    "clientInfo": {
+                        "clientIp": "10.228.237.141",
+                        "deviceId": "68a116b07394858b",
+                        "networktype": "wifi",
+                        "refId": "5866720",
+                        "versionNumber": "1.0",
+                        "versionType": "android"
+                    },
+                    "activeId": "467"
+                },
+                "header": {
+                    "accountID": "c26b007f-c89e-431a-b8cc-493becbdd8a2",
+                    "digitalSign": "7c0a13565e8a110eb4e3875708c056c4",
+                    "reqTime": "1429289137608",
+                    "serviceName": "GetActiveDetailById",
+                    "version": "20111128102912"
+                }
+            }
+        }), // 相当于请求的json
+        success: function (data) {
+            // 将json转string
+            var jsonText = JSON.stringify(data);
+            //alert(jsonText);
+            // 解析json里面的数据
+            var response = data.response;
+            var newsList = data.response.body.newsList;
+            $.each(newsList, function (i, v) {
+                //alert(newsList[i].newsId);
+            });
+            var htmls = "";
+            $.each(newsList, function (i, v) {
+                htmls = htmls + "<div class='item'> <a href='12'>" +
+                "<img style ='  width: 940px; height: 320px;' src='" + newsList[i].thumbimage + "'" + " /></a><!-- /box --></div>";
+            });
+            $("#companyprofile_hpage_slider").html(htmls);
+            initBanner(newsList);
+            // 重定向地址
+            //window.location.href = "../newsdetail/newsdetailpage.html?newsid=1"
+        }
+    });
+}
 
 /**
  * 首页文化banner滚动的js
@@ -62,7 +118,7 @@ function clickGetCluturesList() {
  *
  */
 function initBanner(newsList) {
-    $('#hpage_slider').after('<div id="fsn"><ul id="fs_pagination">').cycle({
+    $('#companyprofile_hpage_slider').after('<div id="fsn"><ul id="fs_pagination">').cycle({
         timeout: 1000, // milliseconds between slide transitions (0 to disable auto advance)
         fx: 'fade', // choose a transition type, ex: fade, scrollUp, shuffle, etc...
         pager: '#fs_pagination', // selector for element to use as pager container
@@ -132,6 +188,47 @@ function clickGetNewsList() {
             });
             $("#newslist").html(htmls);
 
+            // 重定向地址
+            //window.location.href = "../newsdetail/newsdetailpage.html?newsid=1"
+        }
+    })
+}
+
+/**
+ * 招聘
+ */
+function clickGetJoinOurTeam() {
+    $.ajax({
+        // url : "http://yundong.shenghuo365.net/ActiveHandler.ashx",
+        url: "joinourteam.txt",
+        type: "POST",
+        contentType: "application/json", // 必须有
+        dataType: "text", // 表示返回值类型，不必须
+        data: JSON.stringify({
+            "request": {
+                "body": {
+                    "clientInfo": {
+                        "clientIp": "10.228.237.141",
+                        "deviceId": "68a116b07394858b",
+                        "networktype": "wifi",
+                        "refId": "5866720",
+                        "versionNumber": "1.0",
+                        "versionType": "android"
+                    },
+                    "activeId": "467"
+                },
+                "header": {
+                    "accountID": "c26b007f-c89e-431a-b8cc-493becbdd8a2",
+                    "digitalSign": "7c0a13565e8a110eb4e3875708c056c4",
+                    "reqTime": "1429289137608",
+                    "serviceName": "GetActiveDetailById",
+                    "version": "20111128102912"
+                }
+            }
+        }), // 相当于请求的json
+        success: function (data) {
+            var htmls = data;
+            $("#joinourteam_content").html(htmls);
             // 重定向地址
             //window.location.href = "../newsdetail/newsdetailpage.html?newsid=1"
         }
